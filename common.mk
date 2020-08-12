76f857f77b37d69ea02d37aa21e2d1d5b2e6e11f
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    sm8350-common: Create dummy libqti-perfd-client
    * Proprietary perfd blobs can finally be nuked without breaking goodix
    * We could even map the functions to use libperfmgr powerhints in the future
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- common.mk
+++ common.mk
@@ -409,7 +409,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.2.vendor \
+    libqti-perfd-client
+
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
