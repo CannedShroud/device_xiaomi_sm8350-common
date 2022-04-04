ae5b34a9cb25a9434c6d0e755892502e4bf3b0bf
CannedShroud <hypersync112@gmail.com>
2022-04-04 17:53:04 +0530

    lisa/vibrator: Rename to lisa

--- common.mk
+++ common.mk
@@ -550,7 +550,7 @@ PRODUCT_COPY_FILES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator.service \
-    vendor.qti.hardware.vibrator.service.xiaomi_sm8350
+    vendor.qti.hardware.vibrator.service.lisa
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
