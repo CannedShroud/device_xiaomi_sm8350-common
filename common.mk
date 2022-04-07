40902e2d8539fea8ca1bf5472e86324ef9ba907d
CannedShroud <hypersync112@gmail.com>
2022-04-07 21:39:22 +0530

    sm8350-common/perms: Add device name to end of privapp permission file
    to avoid being overriden by other ones from elsewhere like vendor

--- common.mk
+++ common.mk
@@ -522,7 +522,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti-lisa.xml
 
 # Thermal
 PRODUCT_PACKAGES += \
