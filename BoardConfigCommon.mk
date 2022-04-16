405e0632fb9dc8127889eb246fd453f8d8e416fe
CannedShroud <hypersync112@gmail.com>
2022-04-16 12:57:54 +0530

    sm8350-common: build power-libperfmgr

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,6 +180,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PRIVATE_PUBLIC_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/power-libperfmgr
 
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
--- common.mk
+++ common.mk
@@ -443,14 +443,18 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service.lisa-libperfmgr \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.2.vendor \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces
 
 # QMI
 PRODUCT_PACKAGES += \
