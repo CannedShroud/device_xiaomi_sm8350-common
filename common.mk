e5b13e993e26f7e78e0d6bcf614c61d0b5112bc2
CannedShroud <hypersync112@gmail.com>
2022-05-01 13:51:15 +0530

    Revert "sm8350-common: Flatten APEXs for performance"
    This reverts commit ba2f6fd3ab1aeb7c7bf1a8d1ae45db73c999bfea.

--- common.mk
+++ common.mk
@@ -9,6 +9,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product-if-exists, vendor/anx/lisa-camera/anx-lisa_camera.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
